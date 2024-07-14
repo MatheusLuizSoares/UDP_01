dda2688a4183e1414d3dbbe4fd0450e212d53b55
Matheus Luiz Soares <matheusluiz15a@gmail.com>
2024-07-13 22:16:39 -0300

    a parte principal já foi feita

--- Cliente/cliente_udp_fs.py
+++ Cliente/cliente_udp_fs.py
@@ -1,17 +1,17 @@
 import socket
 
-HOST = '192.168.2.245'
+HOST = '192.168.56.1'
 PORT = 50000
 
 server = (HOST, PORT)
 
-ARQ
-
-
 udpSocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-
 while True:
-
-    arquivo=input("Digita o nome do arquvio que tem no servidor")
-    
-    udpSocket.sendto(arquivo, server)
+    lista = ['arquivo_teste.docx', 'batman.txt']
+    print(lista)
+    arquivo = input("Digite o nome do arquivo que está no servidor: ")
+    udpSocket.sendto(arquivo.encode(), server)
+    data, endereco = udpSocket.recvfrom(1024)
+    print(f"Conteúdo do arquivo '{arquivo}': {data.decode()}")
+    with open(arquivo, 'w') as f:
+        f.write(data.decode())
--- Servidor/sevidor_udp_fs.py
+++ Servidor/sevidor_udp_fs.py
@@ -1,11 +1,25 @@
-#gravar resposta, espe
+import socket,os
 
-import socket, datetime
 
 HOST = ''
 PORT = 50000
-clientDir = {}
-
 udpSocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 udpSocket.bind((HOST, PORT))
-print('Server on-line...')
+
+print("Servidor está ouvindo.....")
+while True:
+    data, endereco = udpSocket.recvfrom(1024)
+    arquivo = data.decode()
+    print(f"Pedido recebido para o arquivo: {arquivo}")
+    caminho_arquivo = os.path.join('Arquivo', arquivo)
+
+    try:
+        with open(caminho_arquivo, 'r') as f:
+            conteudo = f.read()
+        
+        udpSocket.sendto(conteudo.encode(), endereco)
+        print(f"Conteúdo do arquivo '{arquivo}' enviado para {endereco}")
+    except FileNotFoundError:
+        mensagem_erro = "Arquivo não encontrado."
+        udpSocket.sendto(mensagem_erro.encode(), endereco)
+        print(f"Arquivo '{arquivo}' não encontrado. Mensagem de erro enviada para {endereco}")
